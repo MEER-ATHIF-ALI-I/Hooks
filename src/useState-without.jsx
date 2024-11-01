6bfe7ac876fdbd844193267d4c60ea210ec6110c
Meer-Athif-Ali-I <meerathifali@gmail.com>
2024-11-01 11:40:26 +0530

    useState with dependency & dependency variable

--- src/useState-without.jsx
+++ src/useState-without.jsx
@@ -14,6 +14,29 @@ function WithoutArray(){
        },2000)
     })
 
+
+    //with dependency array it will be rendered only once
+
+    // useEffect(()=>{
+    //     setTimeout(()=>{
+    //      setCount((count)=>{
+    //          return count +1
+    //      })
+    //     },2000)
+    //  },[])
+
+
+    //useEffect with dependency array which includes variable 
+    // will be rendered whenever the count state changes
+
+    // useEffect(()=>{
+    //     setTimeout(()=>{
+    //      setCount((count)=>{
+    //          return count +1
+    //      })
+    //     },2000)
+    //  },[count])
+    
     return(
         <div>
           <h1>I have rendered {count} times!</h1>
